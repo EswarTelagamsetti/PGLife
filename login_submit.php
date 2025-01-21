992640204ac1bc5ef4633e3f5be2ea508e44a234
EswarTelagamsetti <eswartelagamsetti1234@gmail.com>
2025-01-21 19:20:01 +0530

    db_conn

--- login_submit.php
+++ login_submit.php
@@ -1,7 +1,32 @@
 <?php
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+    $db_hostname = "127.0.0.1";
+    $db_username = "root";
+    $db_password = "";
+    $db_name = "test";
 
-    echo $email . "<br/>";
-    echo $password . "<br/>";
+    $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
+    if (!$conn) {
+        echo "Connection failed: " . mysqli_connect_error();
+        exit;
+    }
+
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+
+    $result = mysqli_query($conn, $sql);
+    if (!$result) {
+        echo "Error: " . mysqli_error($conn);
+        exit;
+    }
+
+    $row = mysqli_fetch_assoc($result);
+    if ($row) {
+        echo "Hello " . $row['name'] . "<br/>";
+    } else {
+        echo "Login Failed<br/>";
+    }
+
+    mysqli_close($conn);
 ?>
