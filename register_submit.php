ddbe4d1510bc6c490fc54c28c27a6073db5907cc
EswarTelagamsetti <eswartelagamsetti1234@gmail.com>
2025-01-21 19:17:13 +0530

    db_conn

--- register_submit.php
+++ register_submit.php
@@ -1,9 +1,30 @@
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+    $db_hostname = "127.0.0.1";
+    $db_username = "root";
+    $db_password = "";
+    $db_name = "test";
+    
+    $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
+    if (!$conn) {
+        echo "Connection failed: " . mysqli_connect_error();
+        exit;
+    }
 
-    echo $name . "<br/>";
-    echo $email . "<br/>";
-    echo $password . "<br/>";
+
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+
+
+    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+
+
+    $result = mysqli_query($conn, $sql);
+    if (!$result) {
+        echo "Error: " . mysqli_error($conn);
+        exit;
+    }
+
+    echo "Registration successful";
+    mysqli_close($conn);
 ?>
